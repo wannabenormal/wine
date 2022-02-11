01320432ca26fcf68e54a98f9b18a0004cae4a9c
Alexey Pesotsky <pesthedog@mail.ru>
2022-02-11 20:12:23 +0400

    Add winery age to template

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+def pluralize(number, words):
+    if all((number % 10 == 1, number % 100 != 11)):
+        return words[0]
+    elif all((2 <= number % 10 <= 4,
+             any((number % 100 < 10, number % 100 >= 20)))):
+        return words[1]
+    return words[2]
 
 
 env = Environment(
@@ -8,7 +18,15 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml'])
 )
 template = env.get_template('template.html')
-rendered_page = template.render()
+
+today = datetime.datetime.today()
+winery_foundation_date = datetime.datetime(year=1920, month=1, day=1)
+delta = today - winery_foundation_date
+winery_age = delta.days // 365
+
+rendered_page = template.render(
+    winery_age=f'{winery_age} {pluralize(winery_age, ["год", "года", "лет"])}'
+)
 
 with open('index.html', 'w', encoding='utf-8') as file:
     file.write(rendered_page)
